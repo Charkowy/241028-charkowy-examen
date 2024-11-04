4579a88e2d59fd8356a60451a436d8e7629c53f0
charkowy <alexischarkowy@gmail.com>
2024-11-04 20:42:45 -0300

    Corrijo prueba

--- websocket/src/main/java/com/example/websocketdemo/service/ChatHandler.java
+++ websocket/src/main/java/com/example/websocketdemo/service/ChatHandler.java
@@ -1,5 +1,6 @@
 package com.example.websocketdemo.service;
 
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Service;
@@ -12,10 +13,17 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class ChatHandler extends TextWebSocketHandler{
 
 	private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<WebSocketSession>();
+	 ArrayList<String> msj = new ArrayList<String>();
+	
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception{
-	sessions.add(session);
+		super.afterConnectionEstablished(session);
+        sessions.add(session);
+
+        for (String mensaje : msj) {
+            session.sendMessage(new TextMessage(mensaje));
+        }
 	}
 	
 	@Override
@@ -25,9 +33,13 @@ public class ChatHandler extends TextWebSocketHandler{
 	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
-		if(message.getPayload() == "websocket") {
-			session.sendMessage(new TextMessage("El protocolo de WebSockets es un protocolo de la capa 7 del modelo OSI, que funciona a través del protocolo TCP. Los WebSocket permiten comunicación a dos vías, desde el cliente hacia el servidor y desde el servidor hacia el cliente sobre una misma conexión.Funciona en los puertos 80 y 443"));
+		String payload = message.getPayload();
+		
+		if (payload.contains("websocket")) {
+            payload += "El protocolo de WebSockets es un protocolo de la capa 7 del modelo OSI, que funciona a través del protocolo TCP. Los WebSocket permiten comunicación a dos vías, desde el cliente hacia el servidor y desde el servidor hacia el cliente sobre una misma conexión.Funciona en los puertos 80 y 443";
 		}
+		msj.add(payload);
+		
 		for (WebSocketSession webSocketSession : sessions) {
 			webSocketSession.sendMessage(message);
 
